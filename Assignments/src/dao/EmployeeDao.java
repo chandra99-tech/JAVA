793063c47884b7baa0d646db62914f683728758d
chandra99-tech <chandra99iit@gmail.com>
2025-10-28 23:41:56 +0530

    Add files via upload

--- Assignments/src/dao/EmployeeDao.java
+++ Assignments/src/dao/EmployeeDao.java
@@ -1,30 +1,5 @@
 package dao;
 
-import java.util.List;
-import java.util.Set;
-
-import bean.Employee;
-
 public interface EmployeeDao {
-
-	boolean save(Employee e);
-
-	List<Employee> showAll();
-
-	Employee findById(int id);
-
-	List<Employee> findByName(String nm);
-
-	List<Employee> findBySalary(double sal);
-
-	boolean removeById(int id);
-
-	boolean removeBySal(double nm);
-
-	boolean modifyBySal(int id, double sal);
-
-	Set<Employee> arrangeById();
-
-	
 
 }
